5d1d816ff19b18a68ef0667fc7d40f643303301a
Matheus-Pontes <pontesm10@outlook.com>
2023-02-06 12:55:35 -0300

    Parte inicial do front. Faltando algumas funcionalidades

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import TodoList from './components/TodoList.vue';
+    
+</script>
+
+<template>
+    <main class="flex flex-col items-center justify-center p-8 px-10 bg-zinc-800 text-white text-center">
+        
+        <div class="lg:w-[48rem] md:w-full md:px-8 sm:w-full sm: px-8">
+            <h1 class="text-2xl py-8 font-bold"> TO DO LIST - VUEJ - TAILWINDCSS </h1>
+    
+            <div class="flex items-center justify-between gap-4 ">
+                <input type="text" v-model="descricaoInput" id="txtToDo" class="input-todoList">
+                <button type="button" id="btnAdicionar" class="bg-green-500 rounded p-4" @click="adicionarNaLista()"> Adicionar</button>
+            </div>
+            
+            <div class="mt-8">
+                <TodoList :listaTodo="lista"/>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+
+export default {
+    components: { TodoList },
+    data() {
+        return {
+            descricaoInput: "",
+            lista: []
+        };
+    },
+    methods: {
+        adicionarNaLista() {
+            if (!this.descricaoInput)
+                alert("Digite algo por favor !!! ");
+            else {
+                this.lista.push({ id: (this.lista.length - 1) + 1, Descricao: this.descricaoInput });
+                this.descricaoInput = "";
+            }
+        }
+    },
+}
+
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+
+   <div class="flex items-center justify-between gap-4">
+        <span class="text-xl flex-1 text-left px-1">Descrição</span>
+
+        <button type="button" class="bg-red-500 rounded px-1 py-4">Excluir todos</button>
+   </div>
+
+    <ul>
+        <li v-for="todo in listaTodo" class="todo-li">
+            
+            {{ todo.Descricao }}
+            
+            <div class="flex items-center justify-between gap-2 ">
+                <button class="btn-edit" @click="abrirEdicao = true">
+                    Editar
+                </button>
+                <button class="btn-excluir">
+                    Excluir
+                </button>
+            </div>
+        </li>
+    </ul>   
+    
+</template>
+
+<script>
+
+    export default {
+        name: 'TodoList',
+        props: ['listaTodo'],
+        data() {
+            return {
+                abrirEdicao: false
+            }
+        },
+        mounted() {
+            console.log(this.listaTodo)
+        }
+    }
+</script>
